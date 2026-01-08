8e62f8c7ca72d682d32889d4ec12f6f48790247e
abdullahhas <abdullah312003@gmail.com>
2026-01-08 19:48:24 +0500

    token gen using exposing endpoint and httpie tool

---
+++ token_gen_exposing_endpoint/token_gen_exposing_endpoint/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path , include
+from api import views
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+
+
+#Creating router object
+router = DefaultRouter()
+
+# Registered StudentViewSet with Router
+router.register('studentapi', views.StudentModelViewSet, basename='student')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('auth/',include('rest_framework.urls', namespace='rest_framework')) , # basic authentication me login ka form type khud hi show ho jata tha lakin session authentication me ye line add kar ke show ho ga wase ni ho ga
+    path('gettoken/',obtain_auth_token)
+]   

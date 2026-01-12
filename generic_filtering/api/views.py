3143e79190069dd32b0d63f7a24eaca51f26f87b
abdullahhas <abdullah312003@gmail.com>
2026-01-12 14:50:11 +0500

    generic filtering

--- generic_filtering/api/views.py
+++ generic_filtering/api/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 from .serializer import StudentSerializer
 from rest_framework.generics import ListAPIView
 from .models import Student
+from django_filters.rest_framework import DjangoFilterBackend  #agar hme globally ni set karna per view set karna he to is tara
+
+
 # Create your views here.
 
 class StudentList(ListAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    def get_queryset(self):
-        user = self.request.user
-        return Student.objects.filter(passby=user)
+    # filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['city']
+
     
 
